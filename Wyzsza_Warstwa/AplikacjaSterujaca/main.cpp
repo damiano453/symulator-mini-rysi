8e859d661dd0d09e436b6590df7577cc34938794
damiano453 <prochaskadamian@gmail.com>
2017-05-08 17:52:09 +0200

    Dizała dojazd do pozycji, problem z obrotem

--- Wyzsza_Warstwa/AplikacjaSterujaca/main.cpp
+++ Wyzsza_Warstwa/AplikacjaSterujaca/main.cpp
@@ -11,18 +11,13 @@ string OutputFile = "/home/damian/git-repos/symulator-mini-rysi/vrep/matlab/Outp
 
 int main(int argc, char *argv[])
 {
-  robot robocik(ControlFile,OutputFile);    ///Tworzymy pojedynczego robota (mozna stworzyc tablice, czyli kilka)
-  robocik.setSpeed(10,10);
-//  cout << endl << 10%180 << endl;
-//  while (1) {
-//      robocik.stopSimulation(0);
-//      cout<<*robocik.getAbsolutePosition()<<"  "<<*(robocik.getAbsolutePosition()+1)<<endl;
-//      getchar();
-//    }
-  robocik.stopSimulation(0);//start
-  irys::sleepcp(50); //500ms
-  cout<< robocik.goStraight(3)<<endl;
-  //robocik.stopSimulation(1);//start
+ robot robocik(ControlFile,OutputFile);    ///Tworzymy pojedynczego robota (mozna stworzyc tablice, czyli kilka)
+ //robocik.setSpeed(10,10);
+ robocik.stopSimulation(0);//start
+ irys::sleepcp(50); //50ms
+ robocik.turn(1);
+ //cout<< robocik.goStraight(1)<<endl;
+ //robocik.stopSimulation(1);//start
   return 0;
 }
 
--- Wyzsza_Warstwa/AplikacjaSterujaca/robot.cpp
+++ Wyzsza_Warstwa/AplikacjaSterujaca/robot.cpp
@@ -30,19 +30,100 @@ void robot::saveControlFile()
 }
 
 /*
+ * Obraca:
+ * 0 -> w prawo
+ * 1 -> w lewo
+ * 2 -> w tył
+ */
+void robot::turn(int dir)
+{
+  int i=0;
+  ///PID params
+  float T = 0.05;
+  float K  = 1400;
+  float Td = 0;
+  float Ti = 0.1;
+
+  float Tv = 150;
+
+  float r2 = 0.0;
+  float r1 = 0.0;
+  float r0 = 0.0;
+
+  float uk_1 =  0.0;
+  float ek_0 = 0.0;
+  float ek_1 = 0.0;
+  float ek_2 = 0.0;
+
+  float u = 0.0;
+  float up = 0.0;
+  float ui = 0.0;
+  float ui_1 =0.0;
+  float ud = 0.0;
+  float uw_1 = 0.0;
+  r2 = K*Td/T;
+  r1 = K*(T/2/Ti - 2*Td/T - 1);
+  r0 = K*(1+ T/2/Ti + Td/T);
+  ///PID params
+  float directionToGet = 0.0;
+  if(dir == 0)  dir = -1; // do algorytmu
+  directionToGet = dir*pi/2;
+  readOutputFile();
+  directionToGet += RobotStat.orientation;
+  if(dir == 2)  dir = 1; // do algorytmu
+  do {
+      readOutputFile();
+      i++;
+      ek_2 = ek_1;
+      ek_1 = ek_0;
+      ek_0 = (directionToGet - RobotStat.orientation);
+      if (ek_0 < 0)
+          dir *= dir;
+
+      up = K* ek_0;
+      ud = K*Td*((ek_0-ek_1)/T);
+      ui = ui_1+K/Ti*T*((ek_1+ek_0)/2);
+
+
+      // anty windup
+      //ui= ui + T/Tv*(uw_1 - uk_1);
+
+      u = up + ui + ud;
+
+      ui_1 = ui;
+      u = r2*ek_2 + r1*ek_1 + r0*ek_0 + uk_1; // <-- tutaj algorytm regulacji
+      uk_1 = u;
+
+      //u windup
+      if (uk_1 > 1000) uw_1 = 1000;
+      else if (uk_1 < -1000) uw_1 = -1000;
+      else uw_1 = uk_1;
+
+      //only u
+      if (u > 1000) u = 1000;
+      else if (u < -1000) u = -1000;
+      else u = u;
+
+      setSpeed(-dir*u,dir*u);
+      irys::sleepcp(30); //5ms
+      std::cout<< std::endl<<std::endl<<"Uchyb = "<<ek_0<<std::endl<<"U = "<<u<<std::endl;
+    } while (ek_0!=0);
+  setSpeed(0,0);
+}
+
+/*
  * Jedzie do przodu o liczbę bloków
  */
-
 float robot::goStraight(int numberOfBlocks)
 {
   int i=0;
   ///PID params
   float T = 0.05;
-  float K  = 10000;
+  float K  = 4000;
   float Td = 0;
-  float Ti = 10000000000000;
+  float Ti = 1.1;
 
-  float Tv = 10000000000000;
+  float Tv = 0.2;
 
   float r2 = 0.0;
   float r1 = 0.0;
@@ -109,9 +190,12 @@ float robot::goStraight(int numberOfBlocks)
 
       setSpeed(u,u);
       std::cout <<"u = "<< u<<std::endl<<"ek = "<<ek_0<<std::endl<<std::endl;
-      irys::sleepcp(50); //50ms
+      irys::sleepcp(5); //5ms
     } while (ek_0!=0);
   setSpeed(0,0);
+  std::cout<<"direction = "<<position<<std::endl;
+  std::cout<<"control error = "<<ek_0<<std::endl;
+  std::cout<<"Position = "<<RobotStat.positionXYZ[position]<<std::endl;
 }
 
 /*
@@ -135,8 +219,8 @@ void robot::setSpeed(int left, int right)
   // To file
   std::stringstream value[2];
   std::string rowToSting;
-  value[0] << left/1000.0;
-  value[1] << right/1000.0;
+  value[0] << left/10000.0;
+  value[1] << right/10000.0;
 
   rowToSting = value[0].str();
   paramsOfRobot.left = rowToSting;
